f6f7fbe3e035e53b2c0fbc47540720655675a6e6
rockwellll <dev.ahmad.khattab@gmail.com>
2025-07-20 20:30:04 +0300

    update nil splatting since it is not support in ruby 3.3.0

--- lib/kleya/browser.rb
+++ lib/kleya/browser.rb
@@ -65,7 +65,7 @@ module Kleya
       def browser
         @browser ||= Ferrum::Browser.new(
           headless: @options.fetch(:headless, true),
-          browser_options: { 'no-sandbox': nil }.merge(**@options[:browser_options]),
+          browser_options: { 'no-sandbox': nil }.merge(**(@options[:browser_options] || {})),
           window_size: @viewport.to_a,
           timeout: @options[:timeout] || 60,
           process_timeout: @options[:process_timeout] || 60,
@@ -74,7 +74,7 @@ module Kleya
       end
 
       def ferrum_options
-        @options.reject { |key, _| %i[width height headless timeout process_timeout].include?(key) }
+        @options.reject { |key, _| %i[width height headless timeout process_timeout browser_options].include?(key) }
       end
   end
 end
